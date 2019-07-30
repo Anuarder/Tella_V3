67e7dbe34c930e6086cb17cb51c758a9203f7603
Anuarder <anuar.ibraev97@gmail.com>
2019-07-30 19:29:45 +0600

    Секция о нас

--- src/components/App/App.vue
+++ src/components/App/App.vue
@@ -3,18 +3,20 @@
         <v-navigation></v-navigation>
         <div class="app">
             <v-header></v-header>
-            asd
+            <v-about></v-about>
         </div>
     </div>
 </template>
 <script>
 import "./app.scss"
-import VNavigation from "@/components/Main/Navigation/Navigation"
+import VNavigation from "@/components/Navigation/Navigation"
 import VHeader from "@/components/Main/Header/Header"
+import VAbout from "@/components/Main/About/About"
 export default {
     components: {
         VNavigation,
-        VHeader
+        VHeader,
+        VAbout
     }
 }
 </script>
---
+++ src/components/Main/About/About.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="about-section" id="#about">
+        <div class="about">
+            <div 
+                class="about__title" 
+                v-html="$t('about.title')">
+            </div>
+            <div class="about__items">
+                <div 
+                    class="about__item"
+                    v-for="(item, i) in advantages"
+                    :key="i">
+                    <img :src="item.image" alt="advantages">
+                    <h1>{{$t(`about.data.${item.key}.title`)}}</h1>
+                    <div class="about__list-container">
+                        <div 
+                            class="about__list"
+                            v-for="j in 3"
+                            :key="j">
+                            <div 
+                                class="bullet" 
+                                :class="!(item.key === 'principle' && j === 3) ? 'circle ': ''">
+                            </div>
+                            <span>
+                                {{$t(`about.data.${item.key}.text${j}`)}}
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+import "./about.scss"
+import technology_image from "@/assets/about-icon_technology.svg"
+import principle_image from "@/assets/about-icon_principle.svg"
+import assurance_image from "@/assets/about-icon_quality.svg"
+export default {
+    data(){
+        return{
+            advantages: [
+                {
+                    image: technology_image,
+                    key: "technology"
+                },
+                {
+                    image: principle_image,
+                    key: "principle"
+                },
+                {
+                    image: assurance_image,
+                    key: "assurance"
+                },
+            ]
+        }
+    }
+}
+</script>
